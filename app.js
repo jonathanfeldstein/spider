a39a79643a2e06f06455562173d32ade633217af
JohnnyF <johnnyf@contactify.ch>
2016-09-07 01:30:10 +0200

    Home added, Company Information added, Contactssearch layout changed, unused pictures deleted

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ var app = angular.module("ProjectC",['ngRoute']);
 
 app.config(function ($routeProvider) { 
   $routeProvider 
+  	.when('/home',{
+  		controller: 'HomeController as ctrl',
+  		templateUrl:'components/home/home.html'
+  	})
+  	.when('/company',{
+  		controller: 'CompanyController as ctrl',
+  		templateUrl:'components/home/company.html'
+  	})
     .when('/contacts', { 
       controller: 'ContactsController as ctrl', 
       templateUrl: 'components/contacts/contacts.html' 
     }) 
     .otherwise({ 
-      redirectTo: '/contacts' 
+      redirectTo: '/home' 
     }); 
 });
