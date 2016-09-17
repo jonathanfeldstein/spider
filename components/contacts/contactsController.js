4779f536b00d647ea892784aba8cf42d9e41b5e7
JohnnyF <johnnyf@contactify.ch>
2016-09-17 23:30:14 +0200

    expanded addContact formular with priority, and changed from dropdown, to radio buttons; new alphabetic search, orderBy nextmeeting; searchbox put  into searchbar; layout change of the searchbar

--- components/contacts/contactsController.js
+++ components/contacts/contactsController.js
@@ -1,27 +1,22 @@
 app.controller('ContactsController', ['$scope','ContactsService','orderByFilter', ContactsController]);
 
-//Controller-Service get All Contacts from User
 function ContactsController($scope, ContactsService, orderBy) {
 
+  //Get all Contacts (Contactbook)
     ContactsService.getContacts()
       .success(function(contactbook){
         $scope.propertyName = 'firstname';
         $scope.reverse = true;
         $scope.contacts = orderBy(contactbook['contacts'], $scope.propertyName, $scope.reverse);
+        $scope.showcontacts=$scope.contacts;
+        var str = "abcdefghijklmnopqrstuvwxyz";
+        $scope.alphabet = str.toUpperCase().split("");
+
+        $scope.activeLetter = '';
+
+
 
       });
-
-    //Organize Information 
-      
-  $scope.sortBy = function(propertyName) {
-    console.log(propertyName);
-    $scope.reverse = (propertyName !== null && $scope.propertyName === propertyName)
-        ? !$scope.reverse : false;
-    $scope.propertyName = propertyName;
-    $scope.contacts = orderBy($scope.contacts, $scope.propertyName, $scope.reverse);
-  
-    };
-
 
   //POST information to server
     $scope.postContact = function (contact) {
@@ -41,4 +36,44 @@ function ContactsController($scope, ContactsService, orderBy) {
               console.error(data);
           });
       };
+
+    //Organize Information 
+      
+  $scope.sortBy = function(propertyName) {
+    $scope.activeLetter = '';
+    console.log(propertyName);
+    var filtered= [];
+    $scope.reverse = (propertyName !== null && $scope.propertyName === propertyName)
+        ? !$scope.reverse : false;
+    $scope.propertyName = propertyName;
+    $scope.contacts = orderBy($scope.contacts, $scope.propertyName, $scope.reverse);
+    $scope.showcontacts=$scope.contacts;
+    };
+  $scope.sortByOneWay = function(propertyName) {
+    $scope.activeLetter = '';    
+    console.log(propertyName);
+    $scope.reverse = false;
+    $scope.propertyName = propertyName;
+    $scope.contacts = orderBy($scope.contacts, $scope.propertyName, $scope.reverse);
+    $scope.showcontacts=$scope.contacts;
+    };
+//Sort by Letter
+        $scope.activateLetter = function(letter) {
+          console.log($scope.propertyName);
+          var sortproperty=$scope.propertyName;
+          var contactsByLetter=[];
+          var letterMatch = new RegExp(letter, 'i');
+          for (var i = 0; i < $scope.contacts.length; i++) {
+              var contact = $scope.contacts[i];
+              
+              if (letterMatch.test(contact[sortproperty].substring(0, 1))) {
+                    contactsByLetter.push(contact);
+                }
+            
+          }
+          $scope.showcontacts = contactsByLetter;
+          $scope.activeLetter = letter
+        }
+
+
 };
