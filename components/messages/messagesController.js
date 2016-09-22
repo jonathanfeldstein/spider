8e48935177275134027377b4ddcccf75668316d8
JohnnyF <johnnyf@contactify.ch>
2016-09-23 00:02:29 +0200

    Messenger: Capable of sorting by length of message and Business/private SIMULTANEOUSLY

--- components/messages/messagesController.js
+++ components/messages/messagesController.js
@@ -8,22 +8,38 @@ function MessagesController($scope, MessagesService) {
         $scope.messages = inbox['messages'];
         $scope.showmessages=$scope.messages;
       });
-
-	$scope.filterbylength="all"
-    $scope.sortByLength=function(message, filterbylength){
-     if(filterbylength=="long")
+    $scope.filterIsBusiness="all";
+	$scope.filterByLength="all";
+    $scope.sortByLength=function(message, filterByLength, filterIsBusiness){
+     if(filterByLength=="long" && filterIsBusiness=="all")
      {
          if(message.prosa.length>=200)
              return true;
          return false;
-     }else if (filterbylength=="short")
+     }else if (filterByLength=="short" && filterIsBusiness=="all")
      {
          if(message.prosa.length<=200)
              return true;
          return false;
-     }else 
+     }else if(filterIsBusiness=="all")
      {
         return true;
+    }
+      else if(filterByLength=="long" && filterIsBusiness!="all")
+     {
+         if(message.prosa.length>=200 && message.business==filterIsBusiness)
+             return true;
+         return false;
+     }else if (filterByLength=="short" && filterIsBusiness!="all")
+     {
+         if(message.prosa.length<=200 && message.business==filterIsBusiness)
+             return true;
+         return false;
+     }else 
+     {
+         if(message.business==filterIsBusiness)
+             return true;
+         return false;
     }
     }
 };
